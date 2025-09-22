29e49decd668ca5862df625941fe1bdcfe0a5a50
Yunseong <dev.yunseong@gmail.com>
2025-09-22 10:28:11 +0900

    feat: add KeyInputSetting script for WebGL mobile keyboard support

---
+++ Assets/Script/KeyInputSetting.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Script
+{
+    public class KeyInputSetting : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            WebGLInput.mobileKeyboardSupport = true;
+        }
+    }
+}
