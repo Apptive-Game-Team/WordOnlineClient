bce85c2645a05821f92abfd31d1232f679fbd745
Yunseong <dev.yunseong@gmail.com>
2025-10-02 22:04:50 +0900

    feat: enhance GuestLoginButton with username input and update login coroutine

--- Assets/Script/LoginScene/GuestLoginButton.cs
+++ Assets/Script/LoginScene/GuestLoginButton.cs
@@ -1,17 +1,25 @@
 using System.Collections;
+using System.Collections.Generic;
 using Script.Data;
 using Script.Global;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GuestLoginButton : AsyncButtonBase
 {
     
+    [SerializeField] private InputField userName;
+    
     private IEnumerator GuestLoginCoroutine()
     {
+        Dictionary<string, string> formData = new Dictionary<string, string>()
+        {
+            {"name", userName.text}
+        };
         
-        using (UnityWebRequest webRequest = new UnityWebRequest(ServerList.AccountServer.url + "/api/members/guest", "POST"))
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(ServerList.AccountServer.url + "/api/members/guest", formData))
         {
             webRequest.downloadHandler = new DownloadHandlerBuffer();
             webRequest.timeout = 10;
