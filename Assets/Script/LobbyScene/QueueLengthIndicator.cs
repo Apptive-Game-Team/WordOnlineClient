0b56cabc0fec326d4906fba657b1a57816d6d4a6
Yunseong <dev.yunseong@gmail.com>
2025-10-02 20:37:04 +0900

    feat: add QueueLengthIndicator to fetch and display queue length

---
+++ Assets/Script/LobbyScene/QueueLengthIndicator.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class QueueLengthIndicator : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _queueLengthText;
+    
+    [Serializable]
+    private class QueueLengthResponse
+    {
+        public int length;
+    }
+    
+    private void Start()
+    {
+        StartCoroutine(UpdateLengthPeriodically());
+    }
+    
+    private IEnumerator UpdateLengthPeriodically()
+    {
+        while (true)
+        {
+            yield return FetchQueueLength();
+            yield return new WaitForSeconds(30f);
+        }
+    }
+    
+    private IEnumerator FetchQueueLength()
+    {
+        using UnityWebRequest webRequest = UnityWebRequest.Get($"{SceneContext.CurrentServer.url}/api/match/length");
+        webRequest.SetRequestHeader("Authorization", "Bearer " + SceneContext.JwtToken);
+        Debug.Log("Fetching queue length from " + $"{SceneContext.CurrentServer.url}/api/match/length" + " with token " + SceneContext.JwtToken);
+        webRequest.downloadHandler = new DownloadHandlerBuffer();
+        
+        yield return webRequest.SendWebRequest();
+        
+        if (webRequest.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError($"Error fetching queue length: {webRequest.error}");
+            yield break;
+        }
+        
+        QueueLengthResponse response = JsonUtility.FromJson<QueueLengthResponse>(webRequest.downloadHandler.text);
+        
+        _queueLengthText.text = $"{response.length}";
+    }
+}
