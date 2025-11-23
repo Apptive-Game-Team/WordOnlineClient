0a4af6ef515bca2572293a98946f516bba736640
Yunseong <dev.yunseong@gmail.com>
2025-11-23 14:11:12 +0900

    feat: add DebugGameResponse class and EnterDebugSessionButton for debug session handling

---
+++ Assets/Script/LobbyScene/Debugger/EnterDebugSessionButton.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using Script.Data;
+using Script.Global;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+namespace Script.LobbyScene.Debugger
+{
+    public class EnterDebugSessionButton : ButtonBase
+    {
+
+        [SerializeField] private string side;
+
+
+        protected override void OnClickButton()
+        {
+            StartCoroutine(EnterDebugSession());
+        }
+
+        private IEnumerator EnterDebugSession()
+        {
+            using var request = UnityWebRequest.PostWwwForm($"http://localhost:7777/api/debug/game/{side}", "");
+            Server.SetAcceptLanguage(request);
+            Server.SetAuthorization(request);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            yield return request.SendWebRequest();
+
+            String json = request.downloadHandler.text;
+            
+            WDebug.Log(json);
+            
+            DebugGameResponse response = JsonUtility.FromJson<DebugGameResponse>(json);
+
+            SceneContext.MatchInfo = MatchedInfoDto.CreateDebugSession(response.sessionId, side, SceneContext.UserID);
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
