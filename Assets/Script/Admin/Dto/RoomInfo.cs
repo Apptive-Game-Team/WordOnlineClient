d3109833da1202c7c3d884ab4206fa257f15a443
Yunseong <dev.yunseong@gmail.com>
2025-12-13 20:42:27 +0900

    feat: add RoomApiClient and data models for room management

---
+++ Assets/Script/Admin/Dto/RoomInfo.cs
@@ -0,0 +1,12 @@
+namespace Script.Admin.Dto
+{
+    [System.Serializable]
+    public class RoomInfo
+    {
+        public string sessionId;
+        public long leftUserId;
+        public long rightUserId;
+        public string serverUrl;
+        
+    }
+}
