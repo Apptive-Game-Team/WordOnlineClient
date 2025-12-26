98b7513a43b7d84be510864c2e36dd7f57f74495
Yunseong <dev.yunseong@gmail.com>
2025-12-26 16:51:19 +0900

    feat: enhance OnAttackSoundPlayer to include servedObject initialization and logging

--- Assets/Script/GameScene/ServedObjectComponent/OnAttack/OnAttackSoundPlayer.cs
+++ Assets/Script/GameScene/ServedObjectComponent/OnAttack/OnAttackSoundPlayer.cs
@@ -1,4 +1,5 @@
 using Script.Data.Sound;
+using Script.Global;
 using UnityEngine;
 
 namespace Script.GameScene.ServedObjectComponent.OnAttack
@@ -13,15 +14,26 @@ namespace Script.GameScene.ServedObjectComponent.OnAttack
         {
             if (audioSource == null)
             {
+                WDebug.Log($"[{gameObject.name}] AudioSource가 없어서 새로 추가합니다.");
                 audioSource = gameObject.AddComponent<AudioSource>();
             }
+            
             audioSource.volume = audioSource.volume * SoundData.gameVolume / 100f;
 
+            
             if (servedObject == null)
             {
                 servedObject = transform.parent.GetComponentInChildren<ServedObject>();
             }
+            
+            if (servedObject == null)
+            {
+                WDebug.LogWarning($"[{transform.parent.name}] ServedObject를 찾지 못했습니다! (GetComponentInChildren 실패)");
+                return; 
+            }
+            
             servedObject.OnAttack += PlayAttackSound;
+            
         }
 
         private void PlayAttackSound()
