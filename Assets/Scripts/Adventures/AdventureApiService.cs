75305e4cb656e2f6e818a31a7bf03417c7293fce
Yunseong <dev.yunseong@gmail.com>
2026-03-18 14:47:47 +0900

    feat: Update PVE request URL and remove unused queue methods in AdventureApiService

--- Assets/Scripts/Adventures/AdventureApiService.cs
+++ Assets/Scripts/Adventures/AdventureApiService.cs
@@ -2,10 +2,7 @@ using System;
 using System.Collections;
 using Data;
 using Data.Sse;
-using Global;
-using LobbyScene;
 using UnityEngine;
-using UnityEngine.Networking;
 
 namespace Adventures
 {
@@ -15,44 +12,8 @@ namespace Adventures
 
         public IEnumerator RequestPVE(long scenarioId, Action<string> callback)
         {
-            sseHandler.StartSse($"{ServerList.MatchingServer.url}/scenarios/{scenarioId}/play", callback);
+            sseHandler.StartSse($"{ServerList.MatchingServer.url}/api/scenarios/{scenarioId}/play", callback);
             yield return null;
         }
-        
-        public IEnumerator RemoveFromQueue()
-        {
-            using var webRequest = new UnityWebRequest($"{ServerList.MatchingServer.url}/api/match/queue/me", "DELETE");
-        
-            Server.SetAcceptLanguage(webRequest);
-            Server.SetAuthorization(webRequest);
-            
-            yield return webRequest.SendWebRequest();
-        }
-    
-        public IEnumerator IsMeInQueue(Action<LobbySceneViewModel.LobbyState> callback)
-        {
-            using var webRequest = new UnityWebRequest($"{ServerList.MatchingServer.url}/api/match/queue/me/exist", "GET");
-        
-            Server.SetAcceptLanguage(webRequest);
-            Server.SetAuthorization(webRequest);
-            
-            yield return webRequest.SendWebRequest();
-
-            if (webRequest.responseCode == 200)
-            {
-                WDebug.Log("User is in queue.");
-                callback(LobbySceneViewModel.LobbyState.Matching);
-            }
-            else if (webRequest.responseCode == 404)
-            {
-                WDebug.Log("Not in queue.");
-                callback(LobbySceneViewModel.LobbyState.Idle);
-            }
-            else
-            {
-                WDebug.LogError($"Error checking queue status: {webRequest.error}");
-            }
-        }
-
     }
 }
