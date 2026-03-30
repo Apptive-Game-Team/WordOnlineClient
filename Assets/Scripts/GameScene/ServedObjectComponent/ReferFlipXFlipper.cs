3f625351e74f88099e3d012890d7965474fab0f2
Yunseong <dev.yunseong@gmail.com>
2026-03-30 13:45:14 +0900

    feat: add ReferFlipXFlipper component for synchronized sprite flipping

---
+++ Assets/Scripts/GameScene/ServedObjectComponent/ReferFlipXFlipper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferFlipXFlipper : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _referRenderer;
+    private SpriteRenderer _renderer;
+    [SerializeField] private Vector3 initialPosition;
+    
+    private void Awake()
+    {
+        if (_referRenderer == null)
+        {
+            Destroy(this);
+        }
+        _renderer = GetComponent<SpriteRenderer>();
+        initialPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        bool flipX = _referRenderer.flipX;
+
+        if (flipX != _renderer.flipX)
+        {
+            _renderer.flipX = flipX;
+            Vector3 currentVect = initialPosition;
+            currentVect.x *= -1;
+            transform.localPosition = currentVect;
+        }
+    }
+}
