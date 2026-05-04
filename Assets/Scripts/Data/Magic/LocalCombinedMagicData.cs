dea065380ed22ab1b95ec227a3b8351ad61ab966
Yunseong <dev.yunseong@gmail.com>
2026-05-04 12:31:19 +0900

    feat(magic): add Crater magic data and associated prefab assets

--- Assets/Scripts/Data/Magic/LocalCombinedMagicData.cs
+++ Assets/Scripts/Data/Magic/LocalCombinedMagicData.cs
@@ -105,6 +105,7 @@ namespace Data.Magic
             new CombinedMagicData(){id = 54, magicName = "Rock Drop", recipe = new () { CardType.Drop, CardType.Rock}, spritePath = "Game/drop/rock_drop"},
             new CombinedMagicData(){id = 55, magicName = "Chicken Commando", recipe = new () { CardType.Drop, CardType.Wind}, spritePath = "Game/drop/chicken_commando"},
             new (){id = 56, magicName = "Overgrowth", recipe = new () { CardType.Explode , CardType.Nature}, spritePath = "Game/explode/overgrowth"},
+            new (){id = 57, magicName = "Crater", recipe = new () { CardType.Build , CardType.Fire}, spritePath = "Game/build/crater"},
         };
         
         public static CombinedMagicData GetCombinedMagicData(string name)
