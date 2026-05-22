4715670ed743b88622cb9341df16ec05c0955c4a
Yunseong <dev.yunseong@gmail.com>
2026-05-22 15:19:13 +0900

    refactor: remove unused config dto types

--- Assets/Scripts/Data/GameConfig/GameConfigData.cs
+++ Assets/Scripts/Data/GameConfig/GameConfigData.cs
@@ -1,42 +1,7 @@
 using System;
-using System.Collections.Generic;
 
 namespace Data.GameConfig
 {
-    [Serializable]
-    public class GameConfigData
-    {
-        public string version;
-        public List<MagicRecipeData> magicRecipes;
-        public List<GameParameterData> parameters;
-        public List<ElementalChartEntry> elementalChart;
-        public List<CardDefinitionData> cards;
-    }
-
-    [Serializable]
-    public class MagicRecipeData
-    {
-        public long id;
-        public string name;
-        public List<string> cards;
-    }
-
-    [Serializable]
-    public class ElementalChartEntry
-    {
-        public string attacker;
-        public string defender;
-        public float multiplier;
-    }
-
-    [Serializable]
-    public class CardDefinitionData
-    {
-        public long id;
-        public string name;
-        public string cardType; // "Type" or "Magic"
-    }
-
     [Serializable]
     public class GameParameterData
     {
