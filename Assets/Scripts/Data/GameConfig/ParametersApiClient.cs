12f909a29b18129ce254b3d6d91c6ee344d5d913
Yunseong <dev.yunseong@gmail.com>
2026-05-22 15:15:48 +0900

    fix: use parameters data endpoint

--- Assets/Scripts/Data/GameConfig/ParametersApiClient.cs
+++ Assets/Scripts/Data/GameConfig/ParametersApiClient.cs
@@ -5,7 +5,7 @@ namespace Data.GameConfig
 {
     public class ParametersApiClient : VersionedApiClient<ParametersResponse>
     {
-        protected override string Endpoint => $"{ServerList.MatchingServer.url}/api/data/config";
+        protected override string Endpoint => $"{ServerList.MatchingServer.url}/api/data/parameters";
 
         protected override void OnSuccessRawJson(string json)
         {
