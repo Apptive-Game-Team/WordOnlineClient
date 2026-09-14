5cb419312e597b7afbcef55eea3a1e32a24c994f
Monolong <83206119+Monolong@users.noreply.github.com>
2026-09-14 11:07:06 +0900

    Revert "merge: stack Boulder Strike on Spirit Bomb"
    This reverts commit 152650307894c9c53aaa8bcd06903390a2869cea, reversing
    changes made to 0c2bcfa11ce129518742009751ab13dc22674017.

--- Assets/Scripts/GameScene/Object/ProjectileSpawner.cs
+++ Assets/Scripts/GameScene/Object/ProjectileSpawner.cs
@@ -20,12 +20,6 @@ namespace GameScene.Object
                 return;
             }
 
-            if (dto.type == "SpiritBombBeam")
-            {
-                SpawnSpiritBombBeam(dto);
-                return;
-            }
-
             if (ShouldSuppressStormStagImpactProjectile(dto))
             {
                 WDebug.Log("Suppressed ElectricShot visual for Storm Stag charge impact.");
@@ -60,14 +54,6 @@ namespace GameScene.Object
                 impactPrefab.transform.rotation);
             impact.transform.localScale *= 0.65f;
             Destroy(impact, dto.duration);
-        }
-
-        private static void SpawnSpiritBombBeam(ProjectileDto dto)
-        {
-            GameObject projectileObject = new GameObject("SpiritBombBeam");
-            SpiritBombBeamProjectile projectile = projectileObject.AddComponent<SpiritBombBeamProjectile>();
-            Destroy(projectileObject, dto.duration);
-            projectile.Init(dto);
         }
 
         private static bool ShouldSuppressStormStagImpactProjectile(ProjectileDto dto)
